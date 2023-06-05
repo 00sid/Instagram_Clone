873d9bc3eb9a732c0794de7df7d5780aca4a4c3f
Shrijan Regmi <your.email@example.com>
2023-06-05 08:10:08 +0545

    fixed scrolling issue on signup screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -54,130 +54,135 @@ class _SignUpScreenState extends State<SignUpScreen> {
             horizontal: 32,
           ),
           width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Flexible(
-                // ignore: sort_child_properties_last
-                child: Container(),
-                flex: 2,
-              ),
-              //logo
-              SvgPicture.asset(
-                "assets/ic_instagram.svg",
-                height: 64,
-                // ignore: deprecated_member_use
-                color: primaryColor,
-              ),
-              const SizedBox(
-                height: 64,
-              ),
-              // circular wigdet to accept and show our selected file
-              Stack(
+          child: SingleChildScrollView(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  _image != null
-                      ? CircleAvatar(
-                          radius: 64, backgroundImage: MemoryImage(_image!))
-                      : const CircleAvatar(
-                          radius: 64,
-                          backgroundImage: NetworkImage(
-                              'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'),
-                        ),
-                  Positioned(
-                      bottom: -10,
-                      left: 80,
-                      child: IconButton(
-                        onPressed: selectImage,
-                        icon: const Icon(Icons.add_a_photo),
-                      ))
-                ],
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              //userName
-              TextFieldInput(
-                textEditingController: _userNameController,
-                hintText: "Enter your name",
-                textInputType: TextInputType.text,
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              //email
-              TextFieldInput(
-                textEditingController: _emailController,
-                hintText: 'Enter your email',
-                textInputType: TextInputType.emailAddress,
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              //password
-              TextFieldInput(
-                textEditingController: _passwordController,
-                hintText: 'Enter your password',
-                textInputType: TextInputType.text,
-                isPass: true,
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              //bio
-              TextFieldInput(
-                textEditingController: _bioController,
-                hintText: "Enter your bio",
-                textInputType: TextInputType.text,
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              //sign in button
-              Button(
-                  loading: _isLoading,
-                  child: "Sign up",
-                  ontap: () {
-                    signUpUser();
-                  }),
-              Flexible(
-                // ignore: sort_child_properties_last
-                child: Container(),
-                flex: 2,
-              ),
-              //transitoning to signup
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8,
-                    ),
-                    child: const Text(
-                      "Already have an account?",
-                    ),
+                  Flexible(
+                    // ignore: sort_child_properties_last
+                    child: Container(),
+                    flex: 2,
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      nextScreen(
-                        context,
-                        const LoginScreen(),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8,
-                      ),
-                      child: const Text(
-                        "Login",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                  //logo
+                  SvgPicture.asset(
+                    "assets/ic_instagram.svg",
+                    height: 64,
+                    // ignore: deprecated_member_use
+                    color: primaryColor,
+                  ),
+                  const SizedBox(
+                    height: 64,
+                  ),
+                  // circular wigdet to accept and show our selected file
+                  Stack(
+                    children: [
+                      _image != null
+                          ? CircleAvatar(
+                              radius: 64, backgroundImage: MemoryImage(_image!))
+                          : const CircleAvatar(
+                              radius: 64,
+                              backgroundImage: NetworkImage(
+                                  'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'),
+                            ),
+                      Positioned(
+                          bottom: -10,
+                          left: 80,
+                          child: IconButton(
+                            onPressed: selectImage,
+                            icon: const Icon(Icons.add_a_photo),
+                          ))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  //userName
+                  TextFieldInput(
+                    textEditingController: _userNameController,
+                    hintText: "Enter your name",
+                    textInputType: TextInputType.text,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  //email
+                  TextFieldInput(
+                    textEditingController: _emailController,
+                    hintText: 'Enter your email',
+                    textInputType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  //password
+                  TextFieldInput(
+                    textEditingController: _passwordController,
+                    hintText: 'Enter your password',
+                    textInputType: TextInputType.text,
+                    isPass: true,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  //bio
+                  TextFieldInput(
+                    textEditingController: _bioController,
+                    hintText: "Enter your bio",
+                    textInputType: TextInputType.text,
+                  ),
+                  const SizedBox(
+                    height: 24,
+                  ),
+                  //sign in button
+                  Button(
+                      loading: _isLoading,
+                      child: "Sign up",
+                      ontap: () {
+                        signUpUser();
+                      }),
+                  Flexible(
+                    // ignore: sort_child_properties_last
+                    child: Container(),
+                    flex: 2,
+                  ),
+                  //transitoning to signup
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+                        child: const Text(
+                          "Already have an account?",
                         ),
                       ),
-                    ),
+                      GestureDetector(
+                        onTap: () {
+                          nextScreen(
+                            context,
+                            const LoginScreen(),
+                          );
+                        },
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 8,
+                          ),
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
